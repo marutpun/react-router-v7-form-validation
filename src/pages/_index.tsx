dd187cc392a39e8895145a5c89a360d13ef98aee
Marut <marutpun@gmail.com>
2025-09-07 13:16:13 +0700

    feat(core): add register form schema

--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -1,5 +1,19 @@
 import { Form } from 'react-router';
 import type { Route } from './+types/_index';
+import * as z from 'zod/v4';
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(6, { error: 'Username must be minimum 6 characters' })
+    .max(30, { error: 'Username must not exceed 30 characters' }),
+  password: z
+    .string()
+    .min(6, { error: 'Password must be minimum 6 characters' })
+    .max(30, { error: 'Password must not exceed 30 characters' }),
+});
+
+type RegisterFormSchema = z.infer<typeof registerFormSchema>;
 
 export default function IndexPage({}: Route.ComponentProps) {
   return (

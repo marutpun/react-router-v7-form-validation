95af997d20d5284027c21bcc9b25b5f3e2edae58
Marut <marutpun@gmail.com>
2025-09-07 12:30:41 +0700

    feat(ui): add register form

--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -1,10 +1,26 @@
 import type { Route } from './+types/_index';
+import { Form } from 'react-router';
 
 export default function IndexPage({}: Route.ComponentProps) {
   return (
     <div className="container">
       <h1>React Router v7 - Form Validation</h1>
       <p>client-side and server-side form validation with Zod and React Hooks Form</p>
+      <h2>Register Form</h2>
+      <Form method="POST">
+        <fieldset>
+          <legend>Account Details</legend>
+          <label>
+            Username
+            <input type="text" name="fullName" required />
+          </label>
+          <label>
+            Password
+            <input type="password" name="password" required />
+          </label>
+        </fieldset>
+        <button type="submit">Register</button>
+      </Form>
     </div>
   );
 }
